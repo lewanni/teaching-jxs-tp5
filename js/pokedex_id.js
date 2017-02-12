0b70f443d3ed292cc75da2c0b18b27339667380b
Sylla <syllamohamedlamine17@gmail.com>
2017-02-12 21:02:16 +0000

    Fait de la question1 à la question7

---
+++ js/pokedex_id.js
@@ -0,0 +1,26 @@
+var pokeApp = angular.module('pokedex', ['ngResource']);
+
+// With this you can inject POKEAPI url wherever you want
+pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+
+pokeApp.config(['$resourceProvider', function($resourceProvider) {
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+pokeApp.controller('c1', data);
+function data($scope){
+	
+	$scope.myBestTeamPokemon = [
+		
+	{id: 1, name: " dracaufeu"},	
+	{id: 2, name: " amphinobi"},
+	{id: 3, name: " junko"},
+	{id: 4, name: " lucario"},
+	{id: 5, name: " drattak"},
+	{id: 6, name: " voltali"}
+		
+	];
+	
+	
+}
+
