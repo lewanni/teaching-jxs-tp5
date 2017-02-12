29f32f619d8f10aaffbc214dc1afe14bd93cc960
Sylla <syllamohamedlamine17@gmail.com>
2017-02-12 21:03:35 +0000

    Fait de la question 8 à la question9

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,21 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller('c1', data);
+function data($scope, $log){
+	
+	$scope.myBestTeamPokemon = [
+		
+	{id: 1, name: " dracaufeu"},	
+	{id: 2, name: " amphinobi"},
+	{id: 3, name: " junko"},
+	{id: 4, name: " lucario"},
+	{id: 5, name: " drattak"},
+	{id: 6, name: " voltali"}
+		
+	];
+	
+	$scope.$log = $log;
+}
+
