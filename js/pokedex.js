8bd7a2191d03602c60d29687a3fcc441740b0daa
13003506 <quang532@gmail.com>
2017-02-14 13:59:37 +0100

    q10 en cours presque fini

--- js/pokedex.js
+++ js/pokedex.js
@@ -7,16 +7,27 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
-pokeApp.controller('c1', data);
+pokeApp.controller('c1', ['$scope', '$log', '$http', data]);
 
-function data($scope, $log) {
+function data($scope, $log, $http) {
 	$scope.myBestTeamPokemon = [
-  	{id: 1, name: "dracaufeu"},
-  	{id: 2, name: "amphinobi"},
-  	{id: 3, name: "junko"},
-  	{id: 4, name: "lucario"},
-  	{id: 5, name: "drattak"},
-  	{id: 6, name: "voltali"}
-	];
-  $scope.$log = $log;
+	{id: 1, name: "dracaufeu"},
+	{id: 2, name: "amphinobi"},
+	{id: 3, name: "junko"},
+	{id: 4, name: "lucario"},
+	{id: 5, name: "drattak"},
+	{id: 6, name: "voltali"}];
+	
+	$scope.$log = $log;
+	for(i = 1; i < 150; i++) {
+		$http.get("http://pokeapi.co/api/v2/pokemon/" + i + "/").then(function(response) {
+			$scope.status = response.status;
+			$scope.lsPoke = "id : " + response.data.id + " name : " + response.data.name;
+		});
+		i++;
+	}
+	/*$http.get("http://pokeapi.co/api/v2/pokemon/" + 1 + "/").then(function(response) {
+		$scope.status = response.status;
+		$scope.lsPoke = "id : " + response.data.id + " name : " + response.data.name " </br>";
+	});*/
 }
