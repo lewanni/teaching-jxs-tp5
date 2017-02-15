91406f60b43a5ebcefb0f15503a28ff6ca71722c
13003506 <quang532@gmail.com>
2017-02-15 12:12:07 +0100

    q10 en cours et en test

--- js/pokedex.js
+++ js/pokedex.js
@@ -10,24 +10,37 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 pokeApp.controller('c1', ['$scope', '$log', '$http', data]);
 
 function data($scope, $log, $http) {
-	$scope.myBestTeamPokemon = [
+	var lsPokes = [];
+	
+	/*[
 	{id: 1, name: "dracaufeu"},
 	{id: 2, name: "amphinobi"},
 	{id: 3, name: "junko"},
 	{id: 4, name: "lucario"},
 	{id: 5, name: "drattak"},
-	{id: 6, name: "voltali"}];
+	{id: 6, name: "voltali"}];*/
 	
 	$scope.$log = $log;
 	for(i = 1; i < 150; i++) {
 		$http.get("http://pokeapi.co/api/v2/pokemon/" + i + "/").then(function(response) {
-			$scope.status = response.status;
-			$scope.lsPoke = "id : " + response.data.id + " name : " + response.data.name;
+			lsPokes.push(response.data);
+			//$scope.lsPoke = lsPokes;
 		});
 		i++;
 	}
+	$scope.myBestTeamPokemon = lsPokes;
 	/*$http.get("http://pokeapi.co/api/v2/pokemon/" + 1 + "/").then(function(response) {
+		var lsPokes = new Array();
 		$scope.status = response.status;
-		$scope.lsPoke = "id : " + response.data.id + " name : " + response.data.name " </br>";
+		lsPokes.push(response.data.name);
+		$scope.lsPoke = lsPokes;
 	});*/
+	//$scope.lsPoke = InfoPoke;
 }
+
+/*pokeApp.factory('InfoPoke', infoPoke);
+
+function infoPoke() {
+	var infoPok = $resource("http://pokeapi.co/api/v1/type/:1/");
+	return infoPok;
+}*/
