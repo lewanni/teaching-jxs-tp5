d9cf8b2458297f221a007200ad464bc31438aea3
lewanni <quang532@gmail.com>
2017-02-26 16:19:01 +0100

    q10 finie

--- js/pokedex.js
+++ js/pokedex.js
@@ -7,35 +7,37 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
-pokeApp.controller('c1', ['$scope', '$log', '$http', data]);
+pokeApp.controller('c1', data);
 
-function data($scope, $log, $http) {
-	var lsPokes = [];
-	
-	/*[
-	{id: 1, name: "dracaufeu"},
-	{id: 2, name: "amphinobi"},
-	{id: 3, name: "junko"},
-	{id: 4, name: "lucario"},
-	{id: 5, name: "drattak"},
-	{id: 6, name: "voltali"}];*/
-	
+function data($scope, $log, $http, POKEAPI) {
+	$scope.lsPoke =
+  [
+  	{id: 1, name: "dracaufeu"},
+  	{id: 2, name: "amphinobi"},
+  	{id: 3, name: "junko"},
+  	{id: 4, name: "lucario"},
+  	{id: 5, name: "drattak"},
+  	{id: 6, name: "voltali"}
+  ];
+
 	$scope.$log = $log;
-	for(i = 1; i < 150; i++) {
-		$http.get("http://pokeapi.co/api/v2/pokemon/" + i + "/").then(function(response) {
-			lsPokes.push(response.data);
-			//$scope.lsPoke = lsPokes;
-		});
-		i++;
-	}
-	$scope.myBestTeamPokemon = lsPokes;
-	/*$http.get("http://pokeapi.co/api/v2/pokemon/" + 1 + "/").then(function(response) {
-		var lsPokes = new Array();
-		$scope.status = response.status;
-		lsPokes.push(response.data.name);
-		$scope.lsPoke = lsPokes;
-	});*/
-	//$scope.lsPoke = InfoPoke;
+
+  $http({
+      method: 'GET',
+      url: POKEAPI + "/api/v2/pokedex/1/"}).then(function successCallback(response) {
+        $scope.lsAllPoke = response.data.pokemon_entries;
+  });
+
+  $scope.infoPoke = function(namePoke, id) {
+    for (var i = 0; i < $scope.lsAllPoke.length; i++) {
+      if ($scope.lsAllPoke[i].pokemon_species.name == namePoke || $scope.lsAllPoke[i].entry_number == id) {
+        return {
+          id: $scope.lsAllPoke[i].entry_number,
+          namePokemon: $scope.lsAllPoke[i].pokemon_species.name
+        }
+      }
+    }
+  };
 }
 
 /*pokeApp.factory('InfoPoke', infoPoke);
